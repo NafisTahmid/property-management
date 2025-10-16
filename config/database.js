3f576212275e56156ffd0859d37f63361ea9db75
unknown <nafistahmid4@gmail.com>
2025-10-16 14:59:28 +0600

    Switched to cloud database commit

--- config/database.js
+++ config/database.js
@@ -1,16 +1,35 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config({});
+
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_NAME,
+//   process.env.DATABASE_USERNAME,
+//   process.env.DATABASE_PASSWORD,
+//   {
+//     host: process.env.HOST,
+//     dialect: "postgres",
+//     logging: false,
+//   }
+// );
+
+// export default sequelize;
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config({});
-// Set up PostgreSQL connection
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+
+// Use environment variable
+const connectionString = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(connectionString, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL
+      rejectUnauthorized: false, // Sometimes needed, depending on provider
+    },
+  },
+  logging: false,
+});
 
 export default sequelize;
--- index.js
+++ index.js
@@ -10,14 +10,18 @@ app.use(express.json());
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/", propertyRoutes);
+// sequelize
+//   .sync({ alter: true })
+//   .then(() => {
+//     console.log("Database synced successfully!");
+//   })
+//   .catch((err) => {
+//     console.error("Error syncing the database:", err);
+//   });
 sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("Database synced successfully!");
-  })
-  .catch((err) => {
-    console.error("Error syncing the database:", err);
-  });
+  .authenticate()
+  .then(() => console.log("Connected to Database!"))
+  .catch((err) => console.error("Unable to connect:", err));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
